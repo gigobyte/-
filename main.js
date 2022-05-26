213d5fb0498753147e7206aa5397af25a693d2ea
gigobyte <gigobest2@gmail.com>
2022-05-26 23:15:01 +0300

    new update UI

--- main.js
+++ main.js
@@ -40,24 +40,14 @@ const loadData = () => {
   })
 }
 
-autoUpdater.on('update-available', () => {
-  updateWin = new BrowserWindow({
-    width: 500,
-    height: 200,
-    frame: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
-  updateWin.on('closed', () => {
-    updateWin = null
-  })
-  updateWin.loadURL(`file://${__dirname}/version.html`)
-})
+// autoUpdater.on('update-available', () => {
+
+// })
 
 autoUpdater.on('download-progress', (progressObj) => {
-  const text = `Налична е нова версия на приложението. Моля изчакайте да бъде изтеглена и автоматично инсталирана. (${progressObj.percent}%)`
+  const text = `Налична е нова версия на приложението.\nМоля изчакайте да бъде изтеглена и автоматично инсталирана. (${Math.floor(
+    progressObj.percent
+  )}%)`
   updateWin.webContents.send('message', text)
 })
 
@@ -71,6 +61,25 @@ autoUpdater.on('update-downloaded', () => {
 app.on('ready', () => {
   autoUpdater.checkForUpdatesAndNotify()
   const lock = app.requestSingleInstanceLock()
+
+  updateWin = new BrowserWindow({
+    width: 550,
+    height: 180,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+  updateWin.on('closed', () => {
+    updateWin = null
+  })
+  updateWin.loadURL(`file://${__dirname}/version.html`)
+
+  setTimeout(() => {
+    const text = `Налична е нова версия на приложението.<br/>Моля изчакайте да бъде изтеглена и автоматично инсталирана. (90%)`
+    updateWin.webContents.send('message', text)
+  }, 500)
 
   if (!lock) {
     app.quit()
